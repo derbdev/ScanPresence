6ece7b626d3c2dbdf4f93a708fa307aa4f83aec4
derbdev <130283588+derbdev@users.noreply.github.com>
2023-04-09 13:35:14 +0000

    Add files via upload

---
+++ ScanPresence/TODO/SessionPage.py
@@ -0,0 +1,13 @@
+#in this page, the user adds a new session to the selected class
+
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy, QPushButton
+
+
+class sessionPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUi()
+
+    def initUi(self):
+        #TODO
+        pass
